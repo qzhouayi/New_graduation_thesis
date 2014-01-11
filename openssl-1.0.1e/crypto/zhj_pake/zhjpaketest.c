f80fd9c9dfd88988f49c788040bb88d586227da4
qzhouayi <huanjiezhou@gmail.com>
2014-01-11 08:26:26 +0800

    set g, h, q as system parameters

--- openssl-1.0.1e/crypto/zhj_pake/zhjpaketest.c
+++ openssl-1.0.1e/crypto/zhj_pake/zhjpaketest.c
@@ -58,34 +58,34 @@ int main(int argc, char **argv)
 	BN_print_fp(stdout, prime);
 	printf("\n");
 	
-    //~ ZHJPAKE_CTX *alice;
-    //~ ZHJPAKE_CTX *bob;
-    //~ BIGNUM *g = BN_new();
-    //~ BIGNUM *q = BN_new();
-    //~ BIGNUM *h = BN_new();
-    //~ BIGNUM *secret = BN_new();
-    //~ BIGNUM *temp = BN_new();
-//~ 
-	//~ //unsigned char md[SHA_DIGEST_LENGTH];
-	//~ //hashpassword(md, "123456");
-	//~ 
-    //~ BN_set_word(q, 19);
-    //~ BN_set_word(g, 10);
-    //~ BN_set_word(h, 13);
-	//~ BN_set_word(secret, 5);
-	//~ printf("Parameters:\n");
-	//~ print_bn("q", q);
-	//~ print_bn("g", g);
-	//~ print_bn("h", h);
-	//~ print_bn("secret", secret);
-//~ 
-    //~ alice = ZHJPAKE_CTX_new(g, q, h, "123456", "Alice", "Bob");
-    //~ printf("alice's secret:");
-    //~ //BN_print_fp(stdout, get_secret(alice));
-    //~ printf("\n");
-    //~ bob = ZHJPAKE_CTX_new(g, q, h, "123456", "Bob", "Alice");
-    //~ //run_zhjpake(alice, bob);
-    //~ 
+    ZHJPAKE_CTX *alice;
+    ZHJPAKE_CTX *bob;
+    BIGNUM *g = BN_new();
+    BIGNUM *q = BN_new();
+    BIGNUM *h = BN_new();
+    BIGNUM *secret = BN_new();
+    BIGNUM *temp = BN_new();
+
+	//unsigned char md[SHA_DIGEST_LENGTH];
+	//hashpassword(md, "123456");
+	
+    BN_set_word(q, 19);
+    BN_set_word(g, 10);
+    BN_set_word(h, 13);
+	BN_set_word(secret, 5);
+	printf("Parameters:\n");
+	print_bn("q", q);
+	print_bn("g", g);
+	print_bn("h", h);
+	print_bn("secret", secret);
+
+    alice = ZHJPAKE_CTX_new(g, q, h, "123456", "Alice", "Bob");
+    printf("alice's secret:");
+    //BN_print_fp(stdout, get_secret(alice));
+    printf("\n");
+    bob = ZHJPAKE_CTX_new(g, q, h, "123456", "Bob", "Alice");
+    //run_zhjpake(alice, bob);
+    
 	return 0;
     }
 
